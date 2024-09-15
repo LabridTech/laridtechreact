c6f1eccb19b7ec8288323a5a9ff7e3338bd4c51f
LabridTech <146828571+LabridTech@users.noreply.github.com>
2024-09-15 22:11:09 +0500

    Update testimonial-info.js

--- src/info/testimonial-info.js
+++ src/info/testimonial-info.js
@@ -1,27 +1,26 @@
-export const  testmonialinfo = [ 
+export const testmonialinfo = [
     {
-        title : 'M luqman' ,
-        index : 1,
-        second : 'Ali Haider',
-        text : 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        title: 'M Luqman',
+        index: 1,
+        second: 'Ali Haider',
+        text: 'We had a major contract of digitalization of our complete buisiness and so far LabridTech has been instrumental in streamlining our operations. Their attention to detail and innovative solutions have significantly boosted our business efficiency.',
     },
     {
-        title : 'Suifyana' ,
-        index : 2,
-        second : 'Ahemd Hassan',
-        text : 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        title: 'Suifyana',
+        index: 2,
+        second: 'Ahmed Hassan',
+        text: 'I have worked with LabridTech on various projects and will continue to do so InshAllah in future because of their budget friendly packages and competent team.',
     },
     {
-        title : 'Fit FLex' ,
-        index : 3,
-        second : 'Ans Chemma',
-        text : 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        title: 'Fit Flex',
+        index: 3,
+        second: 'Ans Chemma',
+        text: 'LabridTech provided the perfect digital solution for our fitness brand. Their creativity and technical skills were a game-changer for our online presence.',
     },
     {
-        title : 'Retail Matrix' ,
-        index : 4 ,
-        second : 'Sulumian Asif',
-        text : 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+        title: 'Retail Matrix',
+        index: 4,
+        second: 'Sulumian Asif',
+        text: 'We partnered with LabridTech to enhance our retail operations, and their solutions exceeded our expectations. The results have been fantastic!',
     }
-
-]
+];
